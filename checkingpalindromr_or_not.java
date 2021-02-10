7e4f41b6e0b5436295d4d4a47a256c5919d2b7ad
johneynoyal <56596887+johneynoyal@users.noreply.github.com>
2021-02-10 09:32:29 +0530

    Update checkingpalindromr_or_not.java

--- checkingpalindromr_or_not.java
+++ checkingpalindromr_or_not.java
@@ -1,32 +1,37 @@
-//check given no is palindrome or not
-import java.io.*;
-import java.util.*;
-import java.lang.Math;
-public class Palindrome_or_not {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number to test: ");
-        int n = scanner.nextInt();
-        int temp = n;
-        int y=0;
-        String z = Integer.toString(n);
-        int c = (int) z.length();
-        c-=1;
-        while(temp !=0)
+
+public class palindrome
+
+{
+
+     public static void main(String []args)
         {
-            int x = temp%10;
-            y = y + (x * (int) Math.pow(10, c));
-            System.out.println(y);
-            c-=1;
-            temp=temp/10;
-        }
-        if(y==n)
-        {
-        	System.out.println("The given number is a Palindrome");
-        }
-        else
-        	System.out.println("The given number is Not a Palindrome");
-        scanner.close();
-    }
-    
+
+            int num=1881,ri=0,rem,original;
+
+            original=num;
+
+           for( ;num !=0;num/=10 )
+
+            {
+
+              rem=num % 10;
+
+              ri=ri*10+rem;
+
+            }
+
+            if(original==ri)
+
+            System.out.println(original+ " is a palindrome.");
+
+            else
+
+            System.out.println(original+ " is not a palindrome.");
+
+         }
+
 }
+ 
+             
+          Output:
+           1881 is a palindrome.
